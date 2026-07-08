360e8f9d09db8a5d326c60b6727d6b23ace9e0ab
Kartikrana001 <kr3511441@gmail.com>
2026-07-08 12:42:28 +0530

    improved login security

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask,render_template,request,redirect, url_for,flash
+from flask_login import LoginManager,UserMixin,login_user,login_required
 import database
 database.create_table()
+database.user_login()
 app = Flask(__name__)
 app.secret_key = "learning_tracker_secret"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self,id,name,email):
+        self.id = id
+        self.name = name
+        self.email = email
+
+@login_manager.user_loader
+def load_user(user_id):
+    data = database.get_user_by_id(user_id)
+    if data:
+        return User(data[0],data[1],data[2])
+    return None
 @app.route("/", methods=["GET", "POST"])
+@login_required
 def home():
     search = request.args.get("search")
 
@@ -34,12 +54,14 @@ def home():
 
 
 @app.route("/delete/<int:id>")
+@login_required
 def delete(id):
     database.delete_topic(id)
     flash("Topic deleted successfully!","danger")
     return redirect(url_for("home"))    
 
 @app.route("/update/<int:id>",methods=['POST','GET'])
+@login_required
 def update(id):
     if request.method == "POST":
         topic = request.form['topic'].strip()
@@ -58,18 +80,46 @@ def update(id):
 
 
 @app.route("/about")
+@login_required
 def about():
     return render_template("about.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['POST','GET'])
 def login():
+    if request.method == "POST":
+        email = request.form['email'].strip()
+        password = request.form['password']
+        data = database.email_login(email,password)
+        if data:
+            current_user = User(data[0],data[1],data[2])
+            login_user(current_user)
+            flash("Login Successful!", "success")
+            return redirect(url_for("home"))
+        flash("Incorrect email or password!","danger")
     return render_template("login.html")
-
-@app.route("/signup")
+@app.route("/signup",methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        name = request.form['name'].strip()
+        email = request.form['email'].strip()
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if any(ch.isdigit() for ch in name) or name == "":
+            flash("Invalid user name!","danger")
+        elif "@" not in email:
+            flash("Invalid email!","danger")
+        elif database.email_exists(email):
+            flash("User already exists!","danger")
+        elif len(password) <8:
+            flash("Password must be greater then 8 digit!","danger")
+        elif confirm_password != password:
+            flash("Password and Confirm password don't matched!","danger")
+        else:
+            database.add_user(name,email,password)
+            flash("Account created successfuly","success")
+            return render_template("login.html")
     return render_template("signup.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,4 +1,39 @@
 import sqlite3
+def user_login():
+    with sqlite3.connect("learning.db") as conn:
+        cursor =conn.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS login_details(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       name TEXT NOT NULL,
+                       email TEXT NOT NULL,
+                       password TEXT NOT NULL
+                       )""")
+
+def add_user(name,email,password):
+    with sqlite3.connect("learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO login_details(name,email,password) VALUES(?,?,?)",(name,email,password))
+
+def email_exists(email):
+    with sqlite3.connect("learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM login_details WHERE email = ?",(email,))
+        result = cursor.fetchone()
+        return (result is not None)
+    
+def email_login(email,password):
+    with sqlite3.connect("learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM login_details WHERE email = ? AND password = ?",(email,password))
+        return cursor.fetchone()
+
+def get_user_by_id(id):
+    with sqlite3.connect("learning.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM login_details WHERE id = ?",(id,))
+        return cursor.fetchone()
+
+
 def create_table():
     with sqlite3.connect("learning.db") as conn:
         cursor = conn.cursor()
